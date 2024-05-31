13cf0ac4e608288b3dfefeff357be71b63ae0100
Gurpreet <dhoat30@gmail.com>
2024-05-31 20:39:22 +1200

    removed meta data

--- app/page.js
+++ app/page.js
@@ -1,32 +1,32 @@
 import HomePage from '@/components/Pages/HomePage'
 
-export const metadata = {
-  title: "Waiheke Prime Taxis Island - Affordable Local Taxi & Tour Services",
-  description: "Offering reliable, friendly taxi rides and personalized island tours. Discover Waiheke's stunning landscapes and attractions with our knowledgeable local drivers.",
-  alternates: {
-    canonical: process.env.url,
-  },
-  openGraph: {
-    title: "Waiheke Prime Taxis Island - Affordable Local Taxi & Tour Services",
-    description: "Offering reliable, friendly taxi rides and personalized island tours. Discover Waiheke's stunning landscapes and attractions with our knowledgeable local drivers.",
-    url: process.env.url,
-    siteName: 'webduel',
-    images: [
-      {
-        url: "/services/taxi.jpg",
-        width: 800,
-        height: 600,
-      },
-      {
-        url: "/services/taxi.jpg",
-        width: 1800,
-        height: 1600,
-      },
-    ],
-    type: 'website',
-  },
+// export const metadata = {
+//   title: "Waiheke Prime Taxis Island - Affordable Local Taxi & Tour Services",
+//   description: "Offering reliable, friendly taxi rides and personalized island tours. Discover Waiheke's stunning landscapes and attractions with our knowledgeable local drivers.",
+//   alternates: {
+//     canonical: process.env.url,
+//   },
+//   openGraph: {
+//     title: "Waiheke Prime Taxis Island - Affordable Local Taxi & Tour Services",
+//     description: "Offering reliable, friendly taxi rides and personalized island tours. Discover Waiheke's stunning landscapes and attractions with our knowledgeable local drivers.",
+//     url: process.env.url,
+//     siteName: 'webduel',
+//     images: [
+//       {
+//         url: "/services/taxi.jpg",
+//         width: 800,
+//         height: 600,
+//       },
+//       {
+//         url: "/services/taxi.jpg",
+//         width: 1800,
+//         height: 1600,
+//       },
+//     ],
+//     type: 'website',
+//   },
 
-}
+// }
 export default async function Home() {
 
 
